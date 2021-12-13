75e0e2d8278c93c1cc61b00b90b64a5c133cb986
Andrea Bergonzo <abergonzo@hubspot.com>
2021-12-13 04:51:52 +0100

    working day9pt2

--- day9/day9.py
+++ day9/day9.py
@@ -11,6 +11,33 @@ def load(filename):
     return matrix
 
 
+def safe_lt(m, p1, p2):
+    i1, j1 = p1
+    i2, j2 = p2
+    if 0 <= i2 < len(m) and 0 <= j2 < len(m[0]):
+        return m[i1][j1] < m[i2][j2]
+    else:
+        return True
+
+
+def is_low_point(m, i, j):
+    is_lp = True
+    for ii, jj in ij_offsets:
+        is_lp = is_lp and safe_lt(m, (i, j), (i + ii, j + jj))
+    return is_lp
+
+
+def sum_risk_levels(m):
+    s = 0
+    for i in range(len(m)):
+        for j in range(len(m[i])):
+            is_lp = is_low_point(m, i, j)
+            if is_lp:
+                # print(f'{m[i][j]} - {[safe_get(m, (i + ii, j + jj)) for ii, jj in [(-1, 0), (0, 1), (1, 0), (0, -1)]]}')
+                s += m[i][j] + 1
+    return s
+
+
 def neighbours_to_consider(m, p, visited):
     l = []
     i, j = p
@@ -27,7 +54,11 @@ def neighbours_to_consider(m, p, visited):
 
 def sum_from_neigh(m, p, visited):
     i, j = p
-    visited[i][j] = True
+    if visited[i][j]:  # because neighbours could be not relevant anymore if reached by another path
+        return 0
+    else:
+        visited[i][j] = True
+
     neighs = neighbours_to_consider(m, (i, j), visited)  # could just return 0 from recursive
     if neighs:
         c = 1
@@ -43,16 +74,11 @@ def get_basin_sizes(m):
     basin_sizes = []
     for i in range(len(m)):
         for j in range(len(m[i])):
-            is_lp = is_low_point(i, j, m)
+            is_lp = is_low_point(m, i, j)
             if is_lp:
                 # print(f'===> LP: {(i, j)} = {m[i][j]}')
-                visited[i][j] = True
-                current_basin_size = 1
-                neighs = neighbours_to_consider(m, (i, j), visited)
-                for neigh in neighs:
-                    current_basin_size += sum_from_neigh(m, neigh, visited)
+                basin_sizes.append(sum_from_neigh(m, (i, j), visited))
                 # print(f'==> Basin Size: {current_basin_size}')
-                basin_sizes.append(current_basin_size)
     return basin_sizes
 
 
@@ -60,33 +86,6 @@ def sum_top_basins(m):
     sizes = get_basin_sizes(m)
     print(sizes)
     return math.prod(sorted(sizes, reverse=True)[:3])
-
-
-def sum_risk_levels(m):
-    s = 0
-    for i in range(len(m)):
-        for j in range(len(m[i])):
-            is_lp = is_low_point(i, j, m)
-            if is_lp:
-                # print(f'{m[i][j]} - {[safe_get(m, (i + ii, j + jj)) for ii, jj in [(-1, 0), (0, 1), (1, 0), (0, -1)]]}')
-                s += m[i][j] + 1
-    return s
-
-
-def is_low_point(i, j, m):
-    is_lp = True
-    for ii, jj in ij_offsets:
-        is_lp = is_lp and safe_lt(m, (i, j), (i + ii, j + jj))
-    return is_lp
-
-
-def safe_lt(m, p1, p2):
-    i1, j1 = p1
-    i2, j2 = p2
-    if i2 < 0 or j2 < 0 or i2 >= len(m) or j2 >= len(m[0]):
-        return True
-    else:
-        return m[i1][j1] < m[i2][j2]
 
 
 def part_one(filename):
@@ -102,4 +101,4 @@ def part_two(filename):
 
 
 # part_one('day9.txt')  # 633 # 15 min (+30 min debug m[-1] is not OOB)
-part_two('day9test.txt')  # ? # 1h+ # correct for test: 9 * 14 * 9 = 1134
+part_two('day9.txt')  # ? # 2h+ # correct for test: 9 * 14 * 9 = 1134
