5eb27f9a5cf7dbc7b9c662cd40aa30ab059c9af0
Andrea Bergonzo <abergonzo@hubspot.com>
2021-12-13 04:53:34 +0100

    day 13 skeleton

---
+++ day13/day13.py
@@ -0,0 +1,20 @@
+def load(filename):
+    l = []
+    with open(filename) as f:
+        for row in f:
+            l.append([a for a in row.strip()])
+    return l
+
+
+def part_one(filename):
+    l = load(filename)
+    print(l)
+
+
+def part_two(filename):
+    l = load(filename)
+    print(l)
+
+
+part_one('day13test.txt')  # ? # ? min
+# part_two('day13test.txt')  # ? # ? min
