b41022707c7f9980ff4ce6ce73a64a7bf3d8c17d
Andrea Bergonzo <abergonzo@hubspot.com>
2021-12-14 19:46:12 +0100

    da14pt2

--- day14/day14.py
+++ day14/day14.py
@@ -23,11 +23,36 @@ def apply_step(t, i):
     return new_t
 
 
-def most_minus_least(t, i, steps):
-    for _ in range(steps):
-        t = apply_step(t, i)
+def apply_step_counter(tc, i):
+    new_tc = Counter()
+    for bi, count in tc.items():
+        first, third = bi
+        second = i[bi]
+        c = Counter({first + second: count, second + third: count})
+        # new_tc.update([first+second])
+        # new_tc.update([second + third])
+        new_tc.update(c)
+    return new_tc
 
-    c = Counter(t)
+
+def pair_counter(t):
+    tc = Counter()
+    prev = t[0]
+    for ch in t[1:]:
+        tc.update([prev + ch])
+        prev = ch
+    return tc
+
+
+def most_minus_least(t, i, steps):
+    tc = pair_counter(t)
+    for _ in range(steps):
+        tc = apply_step_counter(tc, i)
+
+    c = Counter()
+    for bi, count in tc.items():
+        c.update(Counter({bi[1]: count}))
+        # c.update(Counter({bi[0]: count, bi[1]: count}))
     freq = c.most_common()
     return freq[0][1] - freq[-1][1]
 
@@ -39,11 +64,11 @@ def part_one(filename):
 
 
 def part_two(filename):
-    t, l = load(filename)
-    r = most_minus_least(l, steps=10)
+    t, i = load(filename)
+    r = most_minus_least(t, i, steps=40)
     print(r)
 
 
 if __name__ == '__main__':
-    part_one('day14.txt')  # 3555 # 15 min
-    # part_two('day14test.txt')  # ? # ? min
+    # part_one('day14.txt')  # 3555 # 15 min
+    part_two('day14.txt')  # 4439442043739 # 15 min
