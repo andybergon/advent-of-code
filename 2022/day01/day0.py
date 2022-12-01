b8d61c703ac47e63923553ff6c473266b591c67c
Andrea Bergonzo <abergonzo@hubspot.com>
2022-12-01 05:51:44 +0000

    Use heapq

--- 2022/day01/day0.py
+++ 2022/day01/day0.py
@@ -1,16 +1,21 @@
+import heapq
+
+TOP_SIZE = 3
+
+q = []
 with open('input.txt') as f:
     curr_cal = 0
-    # could use priority queue
-    top = [0, 0, 0]
     for line in f:
         v = line.strip()
         if len(v) == 0:
-            i = min(top)
-            if curr_cal > i:
-                top[top.index(i)] = curr_cal
+            # alt: PriorityQueue
+            if len(q) < TOP_SIZE:
+                heapq.heappush(q, curr_cal)
+            else:
+                heapq.heappushpop(q, curr_cal)
             curr_cal = 0
         else:
             curr_cal += int(v)
 
-print(top)
-print(sum(top))
+print(q)
+print(sum(q))
