561c2e4c1be11832f7abbd1ac249322ce8890be1
Andrea Bergonzo <abergonzo@hubspot.com>
2022-12-04 06:08:14 +0000

    minor cleanup

--- 2022/day04/aoc.py
+++ 2022/day04/aoc.py
@@ -1,13 +1,14 @@
 def fully_contained(range, test):
-    low, high = [int(x) for x in range.split('-')]
-    low_t, high_t = [int(x) for x in test.split('-')]
+    low, high = range
+    low_t, high_t = test
     if low <= low_t <= high and low <= high_t <= high:
         return True
     return False
 
+
 def overlap(range, test):
-    low, high = [int(x) for x in range.split('-')]
-    low_t, high_t = [int(x) for x in test.split('-')]
+    low, high = range
+    low_t, high_t = test
     if low_t <= high and low <= high_t:
         return True
     return False
@@ -17,7 +18,11 @@ def part_one(is_sample=False):
     ranges = []
     with open(get_filename(is_sample)) as f:
         for row in f:
-            curr1, curr2 = row.strip().split(',')
+            currs = []
+            for pair in row.strip().split(","):
+                l, r = pair.split("-")
+                currs.append([int(l), int(r)])
+            curr1, curr2 = currs
             if fully_contained(curr1, curr2) or fully_contained(curr2, curr1):
                 ranges.append((curr1, curr2))
     print(len(ranges))
@@ -27,17 +32,20 @@ def part_two(is_sample=False):
     ranges = []
     with open(get_filename(is_sample)) as f:
         for row in f:
-            curr1, curr2 = row.strip().split(',')
-            overlaps = overlap(curr1, curr2) or overlap(curr2, curr1)
-            if overlaps:
+            currs = []
+            for pair in row.strip().split(","):
+                l, r = pair.split("-")
+                currs.append([int(l), int(r)])
+            curr1, curr2 = currs
+            if overlap(curr1, curr2) or overlap(curr2, curr1):
                 ranges.append((curr1, curr2))
     print(len(ranges))
 
 
 def get_filename(is_sample=False):
-    return 'input_sample.txt' if is_sample else 'input.txt'
+    return "input_sample.txt" if is_sample else "input.txt"
 
 
-if __name__ == '__main__':
-    part_one(False)  # ? # 25 min (15 to figure out I was using strings)
-    part_two(False)  # ? # 15 min (was solving gobal overlaps)
+if __name__ == "__main__":
+    part_one(False)  # 547 # 25 min (15 to figure out I was using strings)
+    part_two(False)  # 843 # 15 min (was solving global overlaps)
