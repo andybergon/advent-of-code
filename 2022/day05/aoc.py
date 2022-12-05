e5f132f1c5416c90e2eda2b327b5f6f0a9e16ade
Andrea Bergonzo <abergonzo@hubspot.com>
2022-12-05 06:38:32 +0000

    apply black

--- 2022/day05/aoc.py
+++ 2022/day05/aoc.py
@@ -1,11 +1,11 @@
 def get_filename(is_sample=False):
-    return 'input_sample.txt' if is_sample else 'input.txt'
+    return "input_sample.txt" if is_sample else "input.txt"
 
 
 def get_input(is_sample):
     with open(get_filename(is_sample)) as f:
         start, procedure = f.read().split("\n\n")
-    start_splits = start.split('\n')
+    start_splits = start.split("\n")
     n_cols = int(start_splits[-1][-1])
     curr_row = 1
     stacks = []
@@ -13,29 +13,30 @@ def get_input(is_sample):
         stacks.append([])
     for j in range(1, len(start_splits)):
         i = 0
-        row = start_splits[-1 - j].replace(' ' * 5, ' [.] ').replace('    [', '[.] [')
+        row = start_splits[-1 - j].replace(" " * 5, " [.] ").replace("    [", "[.] [")
 
-        curr_crates = row.split(' ')
+        curr_crates = row.split(" ")
         for crate in curr_crates:
-            if crate == '[.]':
+            if crate == "[.]":
                 i += 1
                 continue
-            stacks[i].append(crate.replace('[', '').replace(']', ''))
+            stacks[i].append(crate.replace("[", "").replace("]", ""))
             i += 1
         curr_row += 1
 
-    procedure = procedure.strip().split('\n')
+    procedure = procedure.strip().split("\n")
     parsed_procedure = []
     for instr in procedure:
         import re
-        m = re.match('move (\d+) from (\d+) to (\d+)', instr)
+
+        m = re.match("move (\d+) from (\d+) to (\d+)", instr)
         parsed_procedure.append([int(v) for v in m.groups()])
 
     return stacks, parsed_procedure
 
 
 def print_output(stacks):
-    s = ''
+    s = ""
     for c in stacks:
         s += c.pop()
     print(s)
@@ -65,6 +66,6 @@ def part_two(is_sample=False):
     print_output(stacks)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     part_one(False)  # SPFMVDTZT # 40 min (20 min parsing file)
     part_two(False)  # ZFSJBPRFP # 10 min
