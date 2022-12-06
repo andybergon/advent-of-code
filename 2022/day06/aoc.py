c9bd31d8d1313263d467a99dadb95bae073bd09f
Andrea Bergonzo <abergonzo@hubspot.com>
2022-12-06 05:13:09 +0000

    day06 cleanup

--- 2022/day06/aoc.py
+++ 2022/day06/aoc.py
@@ -1,32 +1,27 @@
-import queue
-
 def get_filename(is_sample=False):
     return "input_sample.txt" if is_sample else "input.txt"
 
-N = 14
-M = N-1
-# 4 diff chars, most recent, num from beginning to end of 4
-def part_one(is_sample=False):
-    l = []
+
+def find_start(is_sample=False, n=4):
+    m = n - 1
     with open(get_filename(is_sample)) as f:
         seq = f.read().strip()
-        for i in range(M, len(seq)):
-            sub = seq[i-M:i+1]
-            if len(set(sub)) == N:
-                print(i+1)
-                return
+    for i in range(m, len(seq)):
+        sub = seq[i - m : i + 1]
+        if len(set(sub)) == n:
+            print(i + 1)
+            return
+    print(seq)
 
-        print(seq)
+
+def part_one(is_sample=False):
+    find_start(is_sample, 4)
 
 
 def part_two(is_sample=False):
-    l = []
-    with open(get_filename(is_sample)) as f:
-        for row in f:
-            l.append([a for a in row.strip()])
-    print(l)
+    find_start(is_sample, 14)
 
 
 if __name__ == "__main__":
-    part_one(False)  # ? # ? min
-    # part_two(True)  # ? # ? min
+    part_one(False)  # 1034 # 6 min
+    part_two(False)  # 2472 # 1 min
