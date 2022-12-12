d248299f8c6b4d09fd40a3a15164c32ea88d479d
Andrea Bergonzo <abergonzo@hubspot.com>
2022-12-12 06:43:31 +0000

    day12 non recursive

--- 2022/day12/aoc.py
+++ 2022/day12/aoc.py
@@ -1,3 +1,4 @@
+import collections
 import math
 
 
@@ -20,41 +21,31 @@ def elevation(v: str):
     return ord(v)
 
 
-def get_visitables(grid, visited, curr):
+def get_visitables(grid, min_paths, curr):
     i, j = curr
     l = []
     for d in directions:
         di, dj = d
         ii = i + di
         jj = j + dj
-        new_curr = (ii, jj)
-        if new_curr in visited:
-            continue
-        if not (0 <= ii < len(grid) and 0 <= jj < len(grid[0])):
-            continue
-        if elevation(grid[ii][jj]) <= elevation(grid[i][j]) + 1:
-            l.append(new_curr)
+        if (
+            (0 <= ii < len(grid) and 0 <= jj < len(grid[0]))
+            and min_paths[ii][jj] == -math.inf
+            and elevation(grid[ii][jj]) <= elevation(grid[i][j]) + 1
+        ):
+            l.append((ii, jj))
     return l
 
 
-def step(grid, min_paths, visited, path, curr, end):
-    print(path)
-    if curr == end:
-        return path
-    else:
-        paths = []
-        visitables = get_visitables(grid, visited, curr)
+def calc_min_paths(grid, min_paths, q):
+    while q:
+        curr = q.popleft()
+        visitables = get_visitables(grid, min_paths, curr)
         for visitable in visitables:
-            best_to_end = min_paths[curr[0]][curr[1]]
-            if best_to_end < len(new_path):
-
-            new_path = step(grid, min_paths, {*visited, visitable}, [*path, visitable], visitable, end)
-            if new_path:
-                paths.append(new_path)
-        if paths:
-            return min(paths, key=len)
-        else:
-            return None
+            q.append(visitable)
+            ci, cj = curr
+            vi, vj = visitable
+            min_paths[vi][vj] = min_paths[ci][cj] + 1
 
 
 def part_one(is_sample=False):
@@ -63,12 +54,15 @@ def part_one(is_sample=False):
     grid = [[*row] for row in rows]
     curr = find(grid, "S")
     end = find(grid, "E")
-    visited = {curr}
-    min_paths = [-math.inf for row in rows for _ in row]
-    path = [curr]
+    min_paths = [[-math.inf for _ in row] for row in rows]
+    min_paths[curr[0]][curr[1]] = 0
+    q = collections.deque()
+    q.append(curr)
 
-    min_path = step(grid, min_paths, visited, path, curr, end)
-    print(len(min_path))
+    calc_min_paths(grid, min_paths, q)
+
+    ei, ej = end
+    print(min_paths[ei][ej])
 
 
 def part_two(is_sample=False):
@@ -77,5 +71,5 @@ def part_two(is_sample=False):
 
 
 if __name__ == "__main__":
-    part_one(False)  # ? # ? mins
-    # part_two(True)  # ? # ? mins
+    # part_one(False)  # 408 # 1h42m mins
+    part_two(True)  # ? # ? mins
